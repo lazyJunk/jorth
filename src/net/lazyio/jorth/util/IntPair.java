671edd9bc4853b07593889db9197aed5028e243b
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-10-25 01:56:41 +0100

    Clean.

---
+++ src/net/lazyio/jorth/util/IntPair.java
@@ -0,0 +1,20 @@
+package net.lazyio.jorth.util;
+
+public class IntPair {
+
+    public int a;
+    public int b;
+
+    public IntPair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "IntPair{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
+}
