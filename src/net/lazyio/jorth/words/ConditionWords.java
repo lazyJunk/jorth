d0635f1aacf5bd5c55e0f4d259e128dcab1381e8
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-10-25 03:17:53 +0100

    if else conditions.

--- src/net/lazyio/jorth/words/ConditionWords.java
+++ src/net/lazyio/jorth/words/ConditionWords.java
@@ -6,24 +6,33 @@ import static net.lazyio.jorth.Porth.log;
 
 public class ConditionWords {
 
-    public static final IWordParser IF = (tokens, currentIndex, blocks, stack, stackIndex, verbose) -> {
+    public static final IWordParser IF = (tokens, currentIndex, stack, stackIndex, verbose) -> {
         var pop = stack.get(stackIndex - 1);
-        int tmp = currentIndex;
-        var end = blocks.stream().filter(intPair -> intPair.a == tmp).findFirst().get().b;
+
         if (pop == "false") {
-            if (verbose)
-                log("    => [if] value with location %s returned false. Jumping to [end] with location %s.\n", currentIndex, end);
-            currentIndex = end;
-            return new IntPair(currentIndex, stackIndex);
+            if (tokens.get(currentIndex).tokenLocs.containsKey(WordParser.ELSE)) {
+                if (verbose)
+                    log("    => [if] value with location %s returned false. Jumping to [else] with location %s.\n", currentIndex, tokens.get(currentIndex).tokenLocs.get(WordParser.ELSE));
+                return new IntPair(tokens.get(currentIndex).tokenLocs.get(WordParser.ELSE), stackIndex);
+            } else {
+                if (verbose)
+                    log("    => [if] value with location %s returned false. Jumping to [end] with location %s.\n", currentIndex, tokens.get(currentIndex).tokenLocs.get(WordParser.END));
+                return new IntPair(tokens.get(currentIndex).tokenLocs.get(WordParser.END), stackIndex);
+            }
         }
         return new IntPair(currentIndex, stackIndex);
     };
 
-    public static final IWordParser ELSE = (tokens, currentIndex, blocks, stack, stackIndex, verbose) -> {
+    public static final IWordParser ELSE = (tokens, currentIndex, stack, stackIndex, verbose) -> {
+
+        if (tokens.get(currentIndex).tokenLocs.containsKey(WordParser.END)) {
+            return new IntPair(tokens.get(currentIndex).tokenLocs.get(WordParser.END), stackIndex);
+        }
+
         return new IntPair(currentIndex, stackIndex);
     };
 
-    public static final IWordParser END = (tokens, currentIndex, blocks, stack, stackIndex, verbose) -> {
+    public static final IWordParser END = (tokens, currentIndex, stack, stackIndex, verbose) -> {
         return new IntPair(currentIndex, stackIndex);
     };
 }
